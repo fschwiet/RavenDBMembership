d014dc9551020f26994ad972bbba21cbf17880e9
fschwiet <fschwiet@gmail.com>
2011-06-26 16:22:08 -0700

    started alternate integration test approach

---
+++ src/RavenDBMembership.IntegrationTests2/SetupProviders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Windsor;
+using Microsoft.Practices.ServiceLocation;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+
+namespace RavenDBMembership.IntegrationTests2
+{
+    [SetUpFixture]
+    public class SetupProviders
+    {
+        [SetUp]
+        public void PreprareProvider()
+        {
+            var store = new EmbeddableDocumentStore() {RunInMemory = true};
+            store.Initialize();
+
+            var container = new WindsorContainer();
+            container.Register(Castle.MicroKernel.Registration.Component.For<IDocumentStore>().Instance(store));
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+        }
+    }
+}
---
+++ src/RavenDBMembership.IntegrationTests2/can_check_if_a_user_is_in_role.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using NJasmine;
+
+namespace RavenDBMembership.IntegrationTests2
+{
+    public class can_check_if_a_user_is_in_role : GivenWhenThenFixture
+    {
+        static int index = 0;
+
+        public override void Specify()
+        {
+            given("a user", delegate
+            {
+                var roleName = "usernameForRoleTest" + index++;
+
+                var user = arrange(() => Membership.CreateUser(roleName, "password"));
+                 
+                given("roles foo and bar", delegate
+                {
+                    arrange(() => Roles.CreateRole("foo"));
+                    arrange(() => Roles.CreateRole("bar"));
+
+                    when("user is in group foo", delegate
+                    {
+                        arrange(() => Roles.AddUserToRole(roleName, "foo"));
+                        
+                        then("the roles provider indicates user is in role foo", delegate
+                        {
+                            expect(() => Roles.IsUserInRole(roleName, "foo"));
+                        });
+
+                        then("the roles provider indicates user is notin role bar", delegate
+                        {
+                            expect(() => !Roles.IsUserInRole(roleName, "bar"));
+                        });
+                    });
+                });
+            });
+        }
+    }
+}
